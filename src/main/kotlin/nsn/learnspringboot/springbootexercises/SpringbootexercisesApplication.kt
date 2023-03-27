6850e7a74dbe8ba646de738bd29be2b6d7f2341f
ntunyuserge <ntunyuserge@gmail.com>
2023-03-27 14:33:21 +0100

    Initial commit

---
+++ src/main/kotlin/nsn/learnspringboot/springbootexercises/SpringbootexercisesApplication.kt
@@ -0,0 +1,11 @@
+package nsn.learnspringboot.springbootexercises
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringbootexercisesApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringbootexercisesApplication>(*args)
+}
---
+++ src/test/kotlin/nsn/learnspringboot/springbootexercises/SpringbootexercisesApplicationTests.kt
@@ -0,0 +1,13 @@
+package nsn.learnspringboot.springbootexercises
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringbootexercisesApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
